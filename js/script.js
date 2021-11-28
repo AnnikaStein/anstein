40be86167991eb9bb65feb7243df4cea0813e5d3
Annika Stein <annika-stein@t-online.de>
2021-11-29 00:35:25 +0100

    Feat: fill some text; update style with collapsibles, cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Responsive navigation bar (collapse)
 function respNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "navbar" || x.className === "navbar sticky") {
@@ -5,4 +6,21 @@ function respNav() {
   } else {
     x.classList.remove("responsive");
   }
+}
+
+
+// Collabsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("act");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
